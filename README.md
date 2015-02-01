3d7e3272ec9c46b4cd34f290ccf02171f1cd1c47
Peter <peter1010@users.noreply.github.com>
2015-02-01 18:14:29 +0000

    Log everything to file, info above to console as well

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 cd_rip
 ======
 
-Python scripts for ripping CDs to mp3 / oggs
+A Python scripts for ripping CDs to mp3 / oggs.
+
+Usage
+-----
+
+Put CD into drive
+
+type './rip.py' at the command line
+
+It will:
+
+  * read the CD
+  * download the track info from musicbrainz
+  * download the coverart
+  * Create a CUE sheet
+  * Read CD audio and convert to a single FLAC file
+  * Ask User what next..
+  * Next is convert to mp3 or ogg per track
+
--- rip.py
+++ rip.py
@@ -5,5 +5,26 @@ import logging
 
 import rip_lib.main as rip
 
-logging.basicConfig(level=logging.DEBUG)
-rip.main(os.getcwd())
+def config_logging(logfile="log.txt"):
+    if os.path.exists(logfile):
+        os.unlink(logfile)
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    fh.setFormatter(formatter)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(levelname)s - %(message)s'
+    )
+    ch.setFormatter(formatter)
+    root = logging.getLogger()
+    root.addHandler(fh)
+    root.addHandler(ch)
+
+if __name__ == "__main__":
+    config_logging()
+    rip.main(os.getcwd())
